7eabd06bf0c473946145d25607aa60bdac673e11
alparslanustaa <alparslanustaa@gmail.com>
2022-02-01 22:12:58 +0000

    char counter function implement

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// Char counter function 
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on("keyup", function () {
+    const input = $(this).val().length;
+    let remainder = 140 - input;
+    $('form .counter').text(remainder)
+    if (remainder > 0) {
+      $(this).parent().find(".counter").text(remainder).removeClass("red");
+    } else {
+      $(this).parent().find(".counter").text(remainder).addClass("red");
+    }
+  });
+});
