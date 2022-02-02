f82e1da2b0f28db0c2450a2bccf81a1a4e17f8e1
alparslanustaa <alparslanustaa@gmail.com>
2022-02-02 20:32:52 +0000

    dynamic/ form submission&Errors add

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,73 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
-const renderTweets = function (tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    const $tweetsContainer = $("#tweets-container");
-    $tweetsContainer.prepend($tweet);
-  }
-};
+const $tweet = $(`<article class="tweet">Hello world</article>`);
 
 
+$(document).ready(function () {
+  const createTweetElement = function (tweetObject) {
+    let $tweet =
+      /* Your code for creating the tweet element */
+      // ...
 
-const createTweetElement = function (tweet) {
-  let $tweet =
-    /* Your code for creating the tweet element */
-    // ...
-    <article class="tweet">
+      `<article class="tweet">
       <header class="tweet-header">
         <div>
-
-          <h4> Title Tweet </h4>
+      <img src=${tweetObject.user.avatars}>
+      <span class="username">${tweetObject.user.name}</span>
         </div>
-        <span class="username">${tweetObject.user.name}</span>
+        <span class="handle">${tweetObject.user.handle}<span>
       </header>
-      <div>
-        ${tweetObject.content.text}
-      </div>
+      <div class="contentTweet">${tweetObject.content.text}</div>
       <footer class="tweet-footer">
-        <span>${timeago.format(tweetObject.created_at)}</span>
+      <span>${timeago.format(tweetObject.created_at)}</span>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-heart"></i>
           <i class="fas fa-retweet"></i>
         </div>
       </footer>
-    </article>
-
-  return $tweet;
-}
+    </article>`
 
 
-renderTweets(data);
+    return $tweet;
+  }
+
+
+
+  $("#tweet-form").on("submit", function (event) {
+    event.preventDefault();
+
+    if ($("#tweet-text").val().length > 140) {
+      $(".error").slideDown("slow").text("Come on try to make it 140 Char!!");
+
+    } else if (!$("#tweet-text").val()) {
+      $(".error")
+        .slideDown("slow")
+        .text("No empty tweet welcome here!.");
+    } else {
+      event.preventDefault();
+      $(".error").slideUp("slow");
+      const serialiedData = $(this).serialize();
+      $.ajax({
+        method: "POST",
+        url: "http://localhost:8080/tweets/",
+        data: serialiedData,
+        success: function (data) {
+          console.log(data)
+          $("#tweets-container").empty();
+          $("#tweet-form").each(function () {
+            this.reset();
+          });
+          loadTweets();
+          $(".counter").text("140");
+        },
+      });
+    }
+  });
+
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      const $tweetsContainer = $(".tweets-container");
+      $tweetsContainer.prepend($tweet);
+    }
+  };
+
+
+
+  function loadTweets() {
+    $.get("http://localhost:8080/tweets/", function (data) {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
+});
